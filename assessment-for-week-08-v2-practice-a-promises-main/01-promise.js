72b6a4f78ceb44faca1ab7271668322875cee1a4
zachmyu <zach.m.yu@gmail.com>
2022-02-11 23:35:59 -0800

    Added starter for PA-Promises

---
+++ assessment-for-week-08-v2-practice-a-promises-main/01-promise.js
@@ -0,0 +1,29 @@
+const makePromise = require('./test/pot-plant');
+
+const addSoil = makePromise('addSoil');
+const plantSeed = makePromise('plantSeed');
+const growPlant = makePromise('growPlant');
+
+/**
+ * Do not change code above this line.
+ * See README.md for instructions
+
+ ******************************************************************************/
+
+// Your code here
+
+
+/*******************************************************************************
+ * Do not change code below this line.
+ */
+
+if (!exports) {
+  var exports = {};
+}
+(function (exports) {
+  try {
+    exports.potPlant = potPlant;
+  } catch (e) {
+    exports.potPlant = () => { throw e };
+  }
+})(exports);
---
+++ assessment-for-week-08-v2-practice-a-promises-main/02-fetch.js
@@ -0,0 +1,11 @@
+if (!fetch) {
+  var fetch = require('./test/node-fetch')(1);
+}
+
+/**
+ * Do not change code above this line.
+ * See README.md for instructions
+
+ ******************************************************************************/
+
+// Your code here
